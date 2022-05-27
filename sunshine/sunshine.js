2deceac128e5afacb079217357c289ddebe744bc
krzysztof <krzszk96@gmail.com>
2022-05-27 20:31:08 +0200

    version with objects

--- sunshine/sunshine.js
+++ sunshine/sunshine.js
@@ -1,6 +1,46 @@
 
 let builds = document.getElementById('build')
 var blocks = [10,20,10,20,10,30,10]
+var blocksObj = [
+  {height: '10', sunshine: true, color: 'red'},
+  {height: '20', sunshine: true, color: 'red'},
+  {height: '30', sunshine: true, color: 'red'},
+  {height: '20', sunshine: true, color: 'red'},
+  {height: '40', sunshine: true, color: 'red'},
+  {height: '10', sunshine: true, color: 'red'},
+  {height: '20', sunshine: true, color: 'red'}]
+
+
+function checkSunshineObj(array){
+  array[0].sunshine = true
+  array[0].color = 'green'
+
+  for(let j=1; j<array.length; j++){
+    array[j].sunshine = true
+    array[j].color = 'red'
+
+    for(let i=j-1; i>=0; i--){
+      if (array[j].height <= array[i].height)
+        array[j].sunshine = false
+    }     
+    if(array[j].sunshine == true) array[j].color = 'green'
+  }  
+return array 
+}
+
+function displaySunshineObj(array){
+  let html = ``
+  array.forEach(block => {
+    html +=`<div 
+              class="block" 
+              style="height: ${block.height *10 }px; 
+              background-color: ${block.color}">
+              ${block.height}m
+            </div>`
+  })
+  builds.innerHTML = html
+}
+displaySunshineObj(checkSunshineObj(blocksObj))
 
 function checkSunshine(array){
   let check =true
@@ -29,7 +69,7 @@ function checkSunshine(array){
   builds.innerHTML = html
 }
 
-checkSunshine(blocks)
+// checkSunshine(blocks)
 
 const inputs = document.querySelectorAll('#b0, #b1, #b2, #b3, #b4, #b5, #b6')
 
@@ -41,6 +81,9 @@ function updateHeight(e) {
   let number = e.target.id.slice(-1)
   let userinput = e.target.value
   if(userinput <= 0 || userinput > 50) return alert("please put number between 1 and 50")
-  blocks[number]= userinput
-  checkSunshine(blocks)
+  // blocks[number]= userinput
+  // checkSunshine(blocks)
+
+  blocksObj[number].height = userinput
+  displaySunshineObj(checkSunshineObj(blocksObj))
 }
